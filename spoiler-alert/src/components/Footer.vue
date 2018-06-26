664009026a250267f2233411b4a83e259c6f98f6
JRocken <jp.rockenmacher@gmail.com>
2018-06-26 15:20:41 -0600

    footer boilerplate added

---
+++ spoiler-alert/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer>
+        <small>Copywrite 2018 By Spoiler Alert Inc. TH, KR, KJ, GS, JR</small>
+    </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+ /* comming soon! */
+</style>
