13fd1f87ce7e97c5dfd28f1fb5fe28a40bc7ff45
Keith Jones <keithjones@Keiths-MacBook-Pro.local>
2018-06-26 11:16:49 -0600

    created pantry.vue and routed to it

--- spoiler-alert/src/router/index.js
+++ spoiler-alert/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/views/HomePage'
+import Pantry from '@/views/Pantry'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'HomePage',
       component: HomePage
+    },{
+      path: '/pantry',
+      name: 'Pantry',
+      component: Pantry
     }
   ]
 })
---
+++ spoiler-alert/src/views/Pantry.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>welcome to your pantry!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
