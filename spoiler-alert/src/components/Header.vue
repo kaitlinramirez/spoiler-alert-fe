1b8d6cddc30cf15b7da44a339b5ef35e92b2227f
Kaitlin <kaitlin.ramirez@colorado.edu>
2018-06-26 15:13:16 -0600

    added login

--- spoiler-alert/src/components/Header.vue
+++ spoiler-alert/src/components/Header.vue
@@ -3,14 +3,18 @@
         <div>
             <b-navbar type="light" variant="light">
                 <h1>Spoiler Alert!</h1>
+                <Login />
             </b-navbar>
         </div>
     </header>
 </template>
 
 <script>
+import Login from '@/components/Login'
 export default {
-
+  components: {
+    Login
+  }
 }
 </script>
 
---
+++ spoiler-alert/src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="login">
+      <b-nav-form>
+        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Login"/>
+        <b-button size="sm" class="my-2 my-sm-0" type="submit">Login</b-button>
+      </b-nav-form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
