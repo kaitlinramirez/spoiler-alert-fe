26a1fc72f83fa72750b64bf44ae0995cf5cdbaad
Kaitlin <kaitlin.ramirez@colorado.edu>
2018-06-26 11:46:27 -0600

    added show/hide form button

--- spoiler-alert/src/components/Form.vue
+++ spoiler-alert/src/components/Form.vue
@@ -1,18 +1,23 @@
 <template>
-  <form>
-    <label for="food">Food</label>
-    <input type="text" name="food" id="food" value="">
-    <label for="type">Type</label>
-    <input type="text" name="type" id="type" value="">
-    <label for="expiration">Expiration</label>
-    <input type="text" name="expiration" id="expiration" value="">
-    <button type="submit" name="button">Submit</button>
-  </form>
+  <section>
+    <button @click="showForm = !showForm" type="button" >Show Form</button>
+    <form v-if="showForm">
+      <label for="food">Food</label>
+      <input type="text" name="food" id="food" value="">
+      <label for="type">Type</label>
+      <input type="text" name="type" id="type" value="">
+      <label for="expiration">Expiration</label>
+      <input type="text" name="expiration" id="expiration" value="">
+      <button type="submit" name="button">Submit</button>
+    </form>
+  </section>
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    showForm: false
+  })
 }
 </script>
 
