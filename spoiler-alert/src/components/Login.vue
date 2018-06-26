1ac67d62cb9ff22413e1a42f851d464737335eb1
Kaitlin <kaitlin.ramirez@colorado.edu>
2018-06-26 15:40:39 -0600

    login feature

--- spoiler-alert/src/components/Login.vue
+++ spoiler-alert/src/components/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="login">
       <b-nav-form>
-        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Login"/>
+        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="User Name"/>
         <b-button size="sm" class="my-2 my-sm-0" type="submit">Login</b-button>
       </b-nav-form>
     </div>
