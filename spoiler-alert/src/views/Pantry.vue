8d99bbb79494e11863f626587a2e12246d488220
Kaitlin <kaitlin.ramirez@colorado.edu>
2018-06-26 11:39:29 -0600

    added form template

--- spoiler-alert/src/views/Pantry.vue
+++ spoiler-alert/src/views/Pantry.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
         <h1>welcome to your pantry!</h1>
+        <Form />
     </div>
 </template>
 
 <script>
+import Form from '@/components/Form'
 export default {
-
+  components: {
+    Form
+  }
 }
 </script>
 
