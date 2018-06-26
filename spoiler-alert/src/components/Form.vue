559b123e6da0ef933d09688a2f11da4f74da8ed6
Kaitlin <kaitlin.ramirez@colorado.edu>
2018-06-26 11:39:29 -0600

    added form template

---
+++ spoiler-alert/src/components/Form.vue
@@ -0,0 +1,21 @@
+<template>
+  <form>
+    <label for="food">Food</label>
+    <input type="text" name="food" id="food" value="">
+    <label for="type">Type</label>
+    <input type="text" name="type" id="type" value="">
+    <label for="expiration">Expiration</label>
+    <input type="text" name="expiration" id="expiration" value="">
+    <button type="submit" name="button">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
