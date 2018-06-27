9013441048ecaed9a63c9eea1036f523405941ce
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-27 10:22:07 -0600

    add method to get all users, and store the array in data funct

--- spoiler-alert/src/components/Login.vue
+++ spoiler-alert/src/components/Login.vue
@@ -1,22 +1,43 @@
 <template>
-    <div class="login">
-      <b-nav-form>
-        <b-form-input v-model="userName" size="sm" class="mr-sm-2" type="text" placeholder="User Name"/>
-        <b-button @click.prevent="submitUser" size="sm" class="my-2 my-sm-0" type="submit">Login</b-button>
-      </b-nav-form>
-    </div>
+  <div class="login">
+    <b-nav-form>
+      <b-form-input
+        v-model="userName"
+        size="sm"
+        class="mr-sm-2"
+        type="text"
+        placeholder="User Name"/>
+      <b-button
+        type="submit"
+        size="sm"
+        class="my-2 my-sm-0"
+        @click.prevent="submitUser">Login</b-button>
+    </b-nav-form>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      userName: ""
+      userName: "",
+      userTable: null
     }
+  },
+  mounted: function () {
+    this.getUsers();
   },
   methods: {
     submitUser() {
       console.log(this.userName);
+    },
+    getUsers() {
+      const apiUrl =  'https://g-spoiler-alert.herokuapp.com/api/v1/users';
+      fetch(apiUrl)
+        .then(Response => Response.json())
+        .then(Response => {
+          this.userTable = Response.users;
+        })
     }
   }
 }
