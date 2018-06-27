933acdd53fc435713a3f04339aec9d9fd45aa99d
Kaitlin <kaitlin.ramirez@colorado.edu>
2018-06-27 10:08:25 -0600

    progress on login feature

--- spoiler-alert/src/components/Login.vue
+++ spoiler-alert/src/components/Login.vue
@@ -1,15 +1,24 @@
 <template>
     <div class="login">
-      <b-nav-form class="nav-form">
-        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="User Name"/>
-        <b-button size="sm" class="my-2 my-sm-0" type="submit">Login</b-button>
+      <b-nav-form>
+        <b-form-input v-model="userName" size="sm" class="mr-sm-2" type="text" placeholder="User Name"/>
+        <b-button @click.prevent="submitUser" size="sm" class="my-2 my-sm-0" type="submit">Login</b-button>
       </b-nav-form>
     </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      userName: ""
+    }
+  },
+  methods: {
+    submitUser() {
+      console.log(this.userName);
+    }
+  }
 }
 </script>
   
