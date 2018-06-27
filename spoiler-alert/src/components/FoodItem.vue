f087a54cfa4d8a7b57f10a42ae9d8889a5b67448
Keith Jones <keithjones@Keiths-MacBook-Pro.local>
2018-06-27 11:19:22 -0600

    added FoodItem components

---
+++ spoiler-alert/src/components/FoodItem.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <b-card :title="food.name"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2">
+          <img :src="../assets/${food.type}.jpg" alt="">
+    <p class="card-text">
+      {{food.expDate}}
+    </p>
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['food']
+
+}
+</script>
+
+<style>
+div {
+    color: black;
+}
+
+</style>
